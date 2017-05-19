ad753c0c508a6360b0e56d14e8c77dac3a43c63d
Francisco de la Peña <frandelapena@gmail.com>
2017-05-19 10:32:59 +0100

    Add gitignore

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=('link_traits',),
+    packages=find_packages(),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
